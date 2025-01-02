553b3d01a6d22dd0f821efded5bdfbb5403a9144
Vinit-R-Gangurde-Daily <vinitgangurde646@gmail.com>
2025-01-02 14:17:39 +0530

    pushing the code

---
+++ p1.java
@@ -0,0 +1,9 @@
+package FirstRepository;
+
+class p1 {
+ 
+    public static void main(String[] args) {
+        
+        System.out.println("hellow i am p1.java");
+    }
+}
---
+++ p2.java
@@ -0,0 +1,9 @@
+package FirstRepository;
+class p2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("i am in p2.java");
+    }
+    
+}
